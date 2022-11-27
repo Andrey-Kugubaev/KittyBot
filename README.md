cc2ec434c90b6a87b101454f83c08aeeac6f8ee0
Andrey Kugubaev <andrey.kugubaev@yandex.ru>
2022-11-28 00:36:25 +0300

    add Docker, edit README.md

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
-## **Телеграм-бот KittyBot**
-
-Телеграм-бот присылает по запросу котиков, а если не получится, то пришлет собачек.
-Котиков он берет [тут](https://thecatapi.com), а собачек [тут](https://thedogapi.com)
-
-Бот использует библиотеку **python-telegram-bot**
+## **Телеграм-бот для помощи введения канала TheWorldofCats**
+![python](https://img.shields.io/badge/Python-3.9-green)
+![python-telegram-bot](https://img.shields.io/badge/python--telegram--bot-20.0a6-green)
+![PostgreSQL](https://img.shields.io/badge/PostgreSQL-14.6-green)
+<br>
+![telegram](https://img.shields.io/badge/telegram-channel-blue)
+![telegram](https://img.shields.io/badge/DigitalOcean-grey)
+![docker](https://img.shields.io/badge/Docker-grey)
+<br>
+----
+### Описание:
+Телеграм-бот для помощи введения канала _https://t.me/theworldofcats_
+- Бот умеет принимать от подписчиков фотографии;
+- Бот по запросу присылает рандомную фотографию кота, при возникновании ошибки запроса картинки с котом по api, пришлет картинку с собакой
+Фотографии котов запрашиваеются [тут](https://thecatapi.com), а собак [тут](https://thedogapi.com);
+----
+### Планы по развитию:
+- Добавление функции обратной связи;
+- Добавление нового интерактива;
+- Добавление функции загрузки видео.
 
 ### Для запуска бота:
+<details>
+
 - склонируйте проект
 `git clone https://github.com/Andrey-Kugubaev/KittyBot.git`
 - установите и активируйте виртуальное окружение
@@ -14,3 +30,5 @@
 - создайте в корне проекта файл `.env` и укажите в нем token вашего бота `TOKEN=xxx`
 - запустите файл `kittybot.py`
 - наслаждайтесь котиками, или собачками
+
+</details>
--- kittybot.py
+++ kittybot.py
@@ -3,21 +3,64 @@ import os
 
 import requests
 
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import CommandHandler, Updater, Filters, MessageHandler
 from telegram import ReplyKeyboardMarkup
+from telegram import bot
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 secret_token = os.getenv('TOKEN')
+channel_id = os.getenv('CHANNEL')
 updater = Updater(token=secret_token, use_context=True)
 
 logging.basicConfig(
+    level=logging.INFO,
+    filename='main.log',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+)
+logger = logging.getLogger(__name__)
+
 
 URL = 'https://api.thecatapi.com/v1/images/search'
+OTHER_URL = 'https://api.thedogapi.com/v1/images/search'
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+
+    button = ReplyKeyboardMarkup(
+        [['help', 'show cat', 'send cat']],
+        resize_keyboard=True,
+    )
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Hello, {}! How are you? Make a choice, please!'.format(name),
+        reply_markup=button
+    )
+
+
+def info_help(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    urlchannel = 'https://t.me/theworldofcats'
+
+    text = '{}! You can ask me to send a random photo with a cat. ' \
+           'Or you can send me your photo for publishing it in a channel {}!\n' \
+           '\n' \
+           '"show cat"  -  this command will send you a random cat photo\n' \
+           '\n' \
+           '"send cat"  - after this command you can send me a photo will ' \
+           'publish later in my channel. Please add an info in the ' \
+           'description about a place the photo was taken at' \
+           ''.format(name, urlchannel)
+    context.bot.send_message(
+        chat_id=chat.id,
+        text=text,
+    )
 
 
 def get_new_image():
@@ -25,36 +68,62 @@ def get_new_image():
         response = requests.get(URL)
     except Exception as error:
         logging.error(f'Ошибка при запросе к основному API: {error}')
-        new_url = 'https://api.thedogapi.com/v1/images/search'
-        response = requests.get(new_url)
+        response = requests.get(OTHER_URL)
 
     response = response.json()
-    random_cat = response[0].get('url')
-    return random_cat
+    random_photo = response[0].get('url')
+    return random_photo
 
 
 def new_cat(update, context):
     chat = update.effective_chat
-    context.bot.send_photo(chat.id, get_new_image())
-
-
-def wake_up(update, context):
-    chat = update.effective_chat
     name = update.message.chat.first_name
-    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+    text = '{}! Look whom I found for you'.format(name)
+    context.bot.send_photo(chat.id, get_new_image(), caption=text)
 
+
+def send_cat(update, context):
+    chat = update.effective_chat
+    text = "Great! Send me a photo with one or several cats " \
+           "and write me where you took it. Thanks!"
     context.bot.send_message(
         chat_id=chat.id,
-        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
-        reply_markup=button
+        text=text
+    )
+    try:
+        updater.dispatcher.add_handler(MessageHandler(Filters.photo, photo_cat))
+    except:
+        context.bot.send_message(
+            chat_id=chat.id,
+            text='Something went wrong, please upload the photo again',
+        )
+
+
+def photo_cat(update, context):
+    text = update.message.caption
+    image_cat = update.message.photo[-1]
+    context.bot.send_photo(
+        chat_id=channel_id,
+        photo=image_cat,
+        caption=text
     )
 
-    context.bot.send_photo(chat.id, get_new_image())
+
+def unknown(update, context):
+    chat = update.effective_chat
+    text = "Sorry, I didn't understand that command"
+    context.bot.send_message(
+        chat_id=chat.id,
+        text=text
+    )
 
 
 def main():
     updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text('help'), info_help))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text('send cat'), send_cat))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text('show cat'), new_cat))
+    updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
     updater.start_polling()
     updater.idle()
